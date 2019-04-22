254ad20b2dea58e1a65995de4113a0629990783e
Victor Pudeyev <piousbox@gmail.com>
2019-04-22 17:23:13 -0500

    a better clean

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   googleWebClientId: "287149765762-o6jb2tlv93qejrpo5km8ihe2n7rrtuc7.apps.googleusercontent.com",
-  newsitemsPath: 'https://manager.piousbox.com/api/my/newsitems',
+  newsitemsPath: 'http://localhost:3000/api/my/newsitems',
 };
