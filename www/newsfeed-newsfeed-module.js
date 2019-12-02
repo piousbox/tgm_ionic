4a2c155e46b0459e38fe95344f07111c6a8bf725
Victor Pudeyev <piousbox@gmail.com>
2019-12-01 20:08:03 -0600

    built for browser

--- www/newsfeed-newsfeed-module.js
+++ www/newsfeed-newsfeed-module.js
@@ -90,9 +90,10 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
 /* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/common/http */ "./node_modules/@angular/common/fesm5/http.js");
 /* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
-/* harmony import */ var _ionic_native_native_storage_ngx__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @ionic-native/native-storage/ngx */ "./node_modules/@ionic-native/native-storage/ngx/index.js");
-/* harmony import */ var _app_service__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../app-service */ "./src/app/app-service.ts");
-/* harmony import */ var _environments_environment__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../../environments/environment */ "./src/environments/environment.ts");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/dist/index.js");
+/* harmony import */ var _ionic_native_native_storage_ngx__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @ionic-native/native-storage/ngx */ "./node_modules/@ionic-native/native-storage/ngx/index.js");
+/* harmony import */ var _app_service__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../app-service */ "./src/app/app-service.ts");
+/* harmony import */ var _environments_environment__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../../environments/environment */ "./src/environments/environment.ts");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -102,6 +103,42 @@ var __decorate = (undefined && undefined.__decorate) || function (decorators, ta
 var __metadata = (undefined && undefined.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+
 
 
 
@@ -109,12 +146,13 @@ var __metadata = (undefined && undefined.__metadata) || function (k, v) {
 
 
 var NewsfeedPage = /** @class */ (function () {
-    function NewsfeedPage(nativeStorage, appService, router, httpClient) {
+    function NewsfeedPage(nativeStorage, appService, router, httpClient, toastController) {
         var _this = this;
         this.nativeStorage = nativeStorage;
         this.appService = appService;
         this.router = router;
         this.httpClient = httpClient;
+        this.toastController = toastController;
         this.currentUser = null;
         this.newsitems = [];
         this.mainTitle = '';
@@ -124,18 +162,46 @@ var NewsfeedPage = /** @class */ (function () {
             _this.currentUser = data;
             if ('facebook' == data.type) {
                 var params = new _angular_common_http__WEBPACK_IMPORTED_MODULE_1__["HttpParams"]().set('accessToken', data.accessToken);
-                var answer = _this.httpClient.get(_environments_environment__WEBPACK_IMPORTED_MODULE_5__["environment"].newsitemsPath, { params: params });
+                var answer = _this.httpClient.get(_environments_environment__WEBPACK_IMPORTED_MODULE_6__["environment"].newsitemsPath, { params: params });
                 answer.subscribe(function (data) {
                     if (data['newsitems']) {
                         _this.newsitems = data['newsitems'];
                     }
-                }, function (error) {
-                    console.log('+++ error from m3 1-:', JSON.stringify(error));
-                });
+                }, function (error) { return __awaiter(_this, void 0, void 0, function () {
+                    var toast;
+                    return __generator(this, function (_a) {
+                        switch (_a.label) {
+                            case 0:
+                                console.log('+++ error from m3 1-:', JSON.stringify(error));
+                                return [4 /*yield*/, this.toastController.create({
+                                        message: 'The token has expired? Please login.',
+                                        duration: 2000
+                                    })];
+                            case 1:
+                                toast = _a.sent();
+                                toast.present();
+                                return [2 /*return*/];
+                        }
+                    });
+                }); });
             }
-        }, function (error) {
-            console.log('+++ newsfeed doesnt have current_user:', error);
-        });
+        }, function (error) { return __awaiter(_this, void 0, void 0, function () {
+            var toast;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        console.log('+++ newsfeed doesnt have current_user:', error);
+                        return [4 /*yield*/, this.toastController.create({
+                                message: 'You are not logged in - please login.',
+                                duration: 2000
+                            })];
+                    case 1:
+                        toast = _a.sent();
+                        toast.present();
+                        return [2 /*return*/];
+                }
+            });
+        }); });
         router.events.subscribe(function (event) {
             if (event instanceof _angular_router__WEBPACK_IMPORTED_MODULE_2__["NavigationEnd"]) {
                 _this.ngOnInit();
@@ -155,10 +221,11 @@ var NewsfeedPage = /** @class */ (function () {
             template: __webpack_require__(/*! ./newsfeed.page.html */ "./src/app/newsfeed/newsfeed.page.html"),
             styles: [__webpack_require__(/*! ./newsfeed.page.scss */ "./src/app/newsfeed/newsfeed.page.scss")],
         }),
-        __metadata("design:paramtypes", [_ionic_native_native_storage_ngx__WEBPACK_IMPORTED_MODULE_3__["NativeStorage"],
-            _app_service__WEBPACK_IMPORTED_MODULE_4__["AppService"],
+        __metadata("design:paramtypes", [_ionic_native_native_storage_ngx__WEBPACK_IMPORTED_MODULE_4__["NativeStorage"],
+            _app_service__WEBPACK_IMPORTED_MODULE_5__["AppService"],
             _angular_router__WEBPACK_IMPORTED_MODULE_2__["Router"],
-            _angular_common_http__WEBPACK_IMPORTED_MODULE_1__["HttpClient"]])
+            _angular_common_http__WEBPACK_IMPORTED_MODULE_1__["HttpClient"],
+            _ionic_angular__WEBPACK_IMPORTED_MODULE_3__["ToastController"]])
     ], NewsfeedPage);
     return NewsfeedPage;
 }());
