053383319173ee064ba673833ff0dc1ab1e79b91
fallenreaper <wllm.francis@gmail.com>
2020-01-14 00:11:59 -0500

    Needed to implement the Index page to list them all.

--- src/app/CitiesComponent/CitiesIndex.page.ts
+++ src/app/CitiesComponent/CitiesIndex.page.ts
@@ -6,18 +6,26 @@ import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 import { AppService } from '../app-service';
 import { environment } from '../../environments/environment';
+import { CityService } from '../services/city.service';
+import { City } from '../classes/city';
 
 @Component({
   selector: 'app-cities-index',
   templateUrl: './CitiesIndex.page.html',
 })
 export class CitiesIndexPage implements OnInit {
+
+  cities: City[];
+
   constructor(
     private nativeStorage: NativeStorage,
     private appService: AppService,
     private router: Router,
-    public httpClient: HttpClient, 
-  ) {}
+    public httpClient: HttpClient,
+    private _cityService: CityService
+  ) {
+    this._cityService.getAllCities().subscribe( cities => this.cities = cities)
+  }
   navigate(where) {
     this.router.navigate([where]);
   }
--- src/app/CitiesComponent/CitiesShow.page.ts
+++ src/app/CitiesComponent/CitiesShow.page.ts
@@ -6,8 +6,6 @@ import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 import { AppService } from '../app-service';
 import { environment } from '../../environments/environment';
-import { CityService } from '../services/city.service';
-import { City } from '../classes/city';
 
 @Component({
   selector: 'app-cities-show',
@@ -15,7 +13,6 @@ import { City } from '../classes/city';
 })
 export class CitiesShowPage implements OnInit {
   cityName: any = '<No City>';
-  cities: City[];
 
   constructor(
     private nativeStorage: NativeStorage,
@@ -23,10 +20,7 @@ export class CitiesShowPage implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     public httpClient: HttpClient,
-    private _cityService: CityService
-  ) {
-    this._cityService.getAllCities().subscribe( cities => this.cities = cities)
-  }
+  ) { }
   
   ngOnInit() {
     // this.cityName = this.route.snapshot.paramMap.get('city_name');
