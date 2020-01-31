2ab505e8f5b326c5601873d53c0404a4e26ee231
Victor Pudeyev <piousbox@gmail.com>
2020-01-30 23:31:17 -0600

    autobuild

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,7 @@
 export const environment = {
-  api_domain: 'http://localhost:3001',
-  domain: 'tgm.piousbox.com',
+  api_domain: 'https://manager.piousbox.com', // this refers to endpoint. Should be manager.
+  debug: true,
+  domain: 'tgm.piousbox.com', // this refers to data config. should be tgm
   fb_app_id: '3016949928380365',
   googleWebClientId: "287149765762-o6jb2tlv93qejrpo5km8ihe2n7rrtuc7.apps.googleusercontent.com",
   name: 'staging',
