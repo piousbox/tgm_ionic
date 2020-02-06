6e35b436887893826aa03328abff37534dd270ee
Victor Pudeyev <piousbox@gmail.com>
2020-02-05 20:25:12 -0600

    navigation basically works

--- src/app/LayoutSidemenu/LayoutSidemenu.ts
+++ src/app/LayoutSidemenu/LayoutSidemenu.ts
@@ -1,11 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'layout-sidemenu',
   templateUrl: 'LayoutSidemenu.html',
 })
 export class LayoutSidemenu implements OnInit {
-  constructor() {}
+  currentUser:any = {};
+  currentUserStr:string = '<none>';
+
+  env:string = '<none>';
+
+  isApp:boolean = true;
+
+  mainTitle:string = '';
+  mainFooterVisible:string = '';
+  message:string;
+
+  platformList:string = '';
+
+  constructor(
+    private router: Router,
+  ) {}
 
   ngOnInit() {}
+
+  navigate(where) {
+    if ('string' === typeof where) {
+      this.mainTitle = where;
+      this.router.navigate([where]);
+    } else if ('object' === typeof where) {
+      this.mainTitle = where['kind'];
+      let here = '';
+      switch (where['kind']) {
+      case 'report': {
+        here = '/reports';
+        break;
+      } default: {
+      } }
+      this.router.navigate([here]);
+    }
+  }
+
 }
