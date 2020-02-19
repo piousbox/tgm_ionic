7a98b441ad89217d26f0efe7f538dddb185e4af4
Victor Pudeyev <piousbox@gmail.com>
2020-02-18 20:35:38 -0600

    blah

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  api_domain: 'https://manager.piousbox.com', // this refers to endpoint. Should be manager.
+  api_domain: 'https://manager.piousbox.com',
   debug: true,
-  domain: 'tgm.piousbox.com', // this refers to data config. should be tgm
+  domain: 'tgm.piousbox.com',
   fb_app_id: '3016949928380365',
   googleWebClientId: "287149765762-o6jb2tlv93qejrpo5km8ihe2n7rrtuc7.apps.googleusercontent.com",
   name: 'staging',
